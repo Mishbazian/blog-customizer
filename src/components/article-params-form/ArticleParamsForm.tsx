4b1e387aff5390ecdb948901e5d20d485cfe4b40
Mikhail Ivanov <shirokopuh@gmail.com>
2026-04-05 00:28:38 +0700

    feat(articleparamsform): add closing by click outside form
    add useOutsideClickClose hook into component for close form when clicking outside

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,8 +2,9 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import { FormEventHandler, ReactNode, useState } from 'react';
+import { FormEventHandler, ReactNode, useRef, useState } from 'react';
 import clsx from 'clsx';
+import { useOutsideClickClose } from 'src/common/hooks/useOutsideClickClose';
 
 export type TArticleParamsFormProps = {
 	isOpen: boolean;
@@ -13,16 +14,22 @@ export type TArticleParamsFormProps = {
 };
 
 export const ArticleParamsForm = (props: TArticleParamsFormProps) => {
-	const [isOpen, setIsOpen] = useState(props.isOpen);
+	const [isOpen, setIsOpen] = useState<boolean>(props.isOpen);
 	const onArrowBtnClick = () => setIsOpen(!isOpen);
-
+	const rootRef = useRef<HTMLDivElement>(null);
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onChange: setIsOpen,
+	});
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={onArrowBtnClick} />
 			<aside
 				className={clsx(styles.container, {
 					[styles.container_open]: isOpen,
-				})}>
+				})}
+				ref={rootRef}>
 				<form
 					className={styles.form}
 					onSubmit={props.onSubmit}
